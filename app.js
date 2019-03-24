628cbe20b00516e545cfd1721e8178d8a7b740af
Fabusuyi David oluwasegun <dav4thevid@gmail.com>
2019-03-24 22:40:59 +0000

    update dburl

--- app.js
+++ app.js
@@ -16,9 +16,11 @@ var commentRoutes      = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require ("./routes/index")
     
-//mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", {useNewUrlParser: true});
-mongoose.connect("mongodb+srv://new:new@davoucii-ws70a.mongodb.net/test?retryWrites=true", {useNewUrlParser: true});
-// mongo "mongodb+srv://davoucii-ws70a.mongodb.net/test" --username davoucii
+    
+    
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+//mongoose.connect("mongodb+srv://new:new@davoucii-ws70a.mongodb.net/test?retryWrites=true", {useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
